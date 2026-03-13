ad8e712abcb3b9b70cb3ee35178b12eac3c74866
artyom-0705 <artem.nuzhnyi123@gmail.com>
2026-03-13 17:15:15 +0700

    Add files via upload

---
+++ build_F411RE/mpu_i2c.d
@@ -0,0 +1,15 @@
+build_F411RE/mpu_i2c.o: Core/Src/mpu_i2c.c Core/Inc/mpu_i2c.h \
+ CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h \
+ CMSIS/Include/core_cm4.h CMSIS/Include/cmsis_version.h \
+ CMSIS/Include/cmsis_compiler.h CMSIS/Include/cmsis_gcc.h \
+ CMSIS/Include/mpu_armv7.h CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h \
+ Core/Inc/utils.h
+Core/Inc/mpu_i2c.h:
+CMSIS/Devices/STM32F4xx/Inc/STM32F411xE/stm32f411xe.h:
+CMSIS/Include/core_cm4.h:
+CMSIS/Include/cmsis_version.h:
+CMSIS/Include/cmsis_compiler.h:
+CMSIS/Include/cmsis_gcc.h:
+CMSIS/Include/mpu_armv7.h:
+CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h:
+Core/Inc/utils.h:
